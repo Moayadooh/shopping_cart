c2130626ee5f5242c8fd9ccdf2b9c2779752cb55
Moayadooh <moayadsalim97@gmail.com>
2024-08-20 07:43:42 +0400

    create shopping list example

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_cart/auth.dart';
+import 'package:shopping_cart/model/product.dart';
+import 'package:shopping_cart/shopping_list.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      title: "Test",
-      home: Auth(),
+      title: 'Shopping App',
+      home: ShoppingList(
+        products: [
+          Product(name: 'Eggs'),
+          Product(name: 'Flour'),
+          Product(name: 'Chocolate chips'),
+        ],
+      ),
     ),
   );
 }
 
-//https://docs.flutter.dev/ui#changing-widgets-in-response-to-input
+//https://docs.flutter.dev/ui#bringing-it-all-together
