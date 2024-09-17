302e074c2048dbfe0470514703f3ce8c36b57688
Moayadooh <moayadsalim97@gmail.com>
2024-09-17 23:58:23 +0400

    create the tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_cart/tabs.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Tabs(),
+    ),
+  );
+}
---
+++ lib/tabs.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Tabs extends StatefulWidget {
+  const Tabs({super.key});
+
+  @override
+  State<Tabs> createState() => _Tabs();
+}
+
+class _Tabs extends State<Tabs> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Electronic Store'),
+      ),
+      body: const Center(
+        child: Text('Dashboard'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedPageIndex,
+        onTap: _selectPage,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.devices),
+            label: 'Devices',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          /*BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),*/
+        ],
+      ),
+    );
+  }
+}
